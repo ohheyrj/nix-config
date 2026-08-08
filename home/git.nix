dd5c1967469e133495dec0a821f5cc4698613c2c
Richard Annand <richard+git@ohheyrj.co.uk>
2026-08-08 20:18:47 +0100

    feat: add git configuration with gpg signing and lfs support

---
+++ home/git.nix
@@ -0,0 +1,24 @@
+{ ... }:
+
+{
+    programs.git = {
+        enable = true;
+        settings = {
+            user.name = "Richard Annand";
+            user.email = "richard@ohheyrj.co.uk";
+            init.defaultBranch = "main";
+            ghq.root = "/Users/richard/repos";
+            pull.ff = "only";
+            push.autoSetupRemote = true;
+            core.editor = "nvim";
+        };
+        lfs = {
+            enable = true;
+        };
+        signing = {
+            format = "openpgp";
+            signer = "gpg";
+            signByDefault = true;
+        };
+    };
+}
