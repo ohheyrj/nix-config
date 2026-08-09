b3649b4050e0ef548d206a8c822882181fbc9833
Richard Annand <richard+git@ohheyrj.co.uk>
2026-08-09 20:19:24 +0100

    feat: add GitHub MCP server with sops-managed PAT

--- home/mcp.nix
+++ home/mcp.nix
@@ -2,6 +2,7 @@
 
 {
   sops.secrets.context7_api_key = { };
+  sops.secrets.github_personal_access_token = { };
   programs.mcp = {
     enable = true;
     servers = {
@@ -24,6 +25,21 @@
           file = config.sops.secrets.context7_api_key.path;
         };
       };
+      github = {
+        command = "podman";
+        args = [
+          "run"
+          "-i"
+          "--rm"
+          "-e"
+          "GITHUB_PERSONAL_ACCESS_TOKEN"
+          "ghcr.io/github/github-mcp-server"
+        ];
+        env.GITHUB_PERSONAL_ACCESS_TOKEN = {
+          file = config.sops.secrets.github_personal_access_token.path;
+        };
+      };
     };
   };
 }
+
