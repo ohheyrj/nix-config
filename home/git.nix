97da387c7a19b4170c9e2e95be4d0d5055c3bd06
Richard Annand <richard+git@ohheyrj.co.uk>
2026-08-10 21:25:37 +0100

    feat: add GPG signing key to git config

--- home/git.nix
+++ home/git.nix
@@ -4,8 +4,11 @@ _:
   programs.git = {
     enable = true;
     settings = {
-      user.name = "Richard Annand";
-      user.email = "richard@ohheyrj.co.uk";
+      user = {
+        name = "Richard Annand";
+        email = "richard@ohheyrj.co.uk";
+        signingKey = "7FA00C301979455214E4294865212690666E56A5!";
+      };
       init.defaultBranch = "main";
       ghq.root = "/Users/richard/repos";
       pull.ff = "only";
