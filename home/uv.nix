e5cb38a38a27a8ffc3de076d8268d6ad598fa530
Richard Annand <richard@ohheyrj.co.uk>
2026-08-18 13:13:11 +0100

    refactor(home/uv): replace declarative uv tool packages with activation script
    Switch from `programs.uv.tool.packages` to a home-manager activation
    entry that runs `uv tool install graphifyy` after `writeBoundary`.
    This provides more control over the tool installation process.

--- home/uv.nix
+++ home/uv.nix
@@ -1,10 +1,9 @@
-_:
+{ pkgs, lib, ... }:
 
 {
-  programs.uv = {
-    enable = true;
-    tool.packages = [
-      "graphify"
-    ];
-  };
+  programs.uv.enable = true;
+
+  home.activation.uvToolInstall = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    ${pkgs.uv}/bin/uv tool install graphifyy
+  '';
 }
